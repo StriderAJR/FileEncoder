0e5bf55fa1542dcb8568aae56c170c89d425370e
Alexander Kirsanov <alexander.a.kirsanov@gmail.com>
2021-05-06 14:32:06 +0500

    temp

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.IO.Compression;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Windows.Forms;
 
@@ -9,21 +10,6 @@ using System.Windows.Forms;
 
 namespace FileEncoder
 {
-    /// <summary>
-    /// Источник получения данных
-    /// </summary>
-    public enum Source
-    {
-        /// <summary>
-        /// Из файла
-        /// </summary>
-        File,
-        /// <summary>
-        /// Из буфера обмена
-        /// </summary>
-        Buffer
-    }
-    
     /// <summary>
     /// Общий класс для всех команд
     /// </summary>
@@ -41,30 +27,16 @@ namespace FileEncoder
     public abstract class ConvertCommand : Command
     {
         /// <summary>
-        /// Источник данных
-        /// </summary>
-        protected Source Source { get; }
-        /// <summary>
         /// Путь до файла, указанный пользователем
         /// </summary>
         protected string FilePath { get; }
-        /// <summary>
-        /// Путь до оригинального файла 
-        /// </summary>
-        protected string BinaryFilePath;
-        /// <summary>
-        /// Путь до файла, в котором хранится base64 строка, полученная из оригинального файла
-        /// </summary>
-        protected string Base64FilePath;
 
         /// <summary>
         /// Конструктор
         /// </summary>
-        /// <param name="source">Источник данных</param>
         /// <param name="filePath">Путь до файла, с которым будет работать команда</param>
-        protected ConvertCommand(Source source, string filePath)
+        protected ConvertCommand(string filePath)
         {
-            Source = source;
             FilePath = filePath;
         }
     }
@@ -74,70 +46,43 @@ namespace FileEncoder
     /// </summary>
     public class EncodeCommand : ConvertCommand
     {
-        public EncodeCommand(Source source, string filePath) : base(source, filePath) { }
+        public EncodeCommand(string filePath) : base(filePath) { }
 
         /// <inheritdoc/>
         public override void Execute()
         {
-            Base64FilePath = GetBase64FilePath();
-            BinaryFilePath = FilePath;
-
-            if (!File.Exists(BinaryFilePath))
+            if (!File.Exists(FilePath))
             {
-                throw new Exception($"File {BinaryFilePath} not found");
+                throw new Exception($"File {FilePath} not found");
             }
-            
-            Console.WriteLine($"Reading file {BinaryFilePath}...");
-            Byte[] bytes = File.ReadAllBytes(BinaryFilePath);
-            Console.WriteLine("Converting to base64 string...");
-            string base64String = Convert.ToBase64String(bytes);
-            Console.WriteLine("Compressing...");
-            byte[] compressedFile = Compressor.Zip(base64String);
-            Console.WriteLine("Converting to base64 string compressed file...");
-            string compressedBase64 = Convert.ToBase64String(compressedFile);
-            
-            Console.WriteLine("Saving...");
-            WriteBase64String(compressedBase64);
-            Console.WriteLine("Done.");
-        }
 
-        /// <summary>
-        /// Получить путь до файла, который будет хранить base64 строку
-        /// </summary>
-        /// <param name="filePath">Путь до оригинального файла</param>
-        /// <returns>Путь до файла с base64 строкой</returns>
-        private string GetBase64FilePath()
-        {
-            string outputFileName = Path.GetFileName(FilePath).Replace(".", "_") + ".txt";
-            return Path.Combine(new[] {Path.GetPathRoot(FilePath), outputFileName});
+            string base64FilePath = FilePath + ".base64";
+            string compressedBase64FilePath = base64FilePath + ".compressed";
+            string b64CompressedBase64FilePath = compressedBase64FilePath + ".base64";
+            
+            Console.WriteLine($"Reading file {FilePath} and converting to base64 string...");
+            Base64Converter.FileToBase64String(FilePath, base64FilePath);
+            Console.WriteLine("Compressing...");
+            Compressor.Zip(base64FilePath, compressedBase64FilePath);
+            Base64Converter.FileToBase64String(compressedBase64FilePath, compressedBase64FilePath + ".base64");
+            string b64 = File.ReadAllText(b64CompressedBase64FilePath);
+            SaveToClipboard(b64);
+
+            // Console.WriteLine("Converting compressed file to base64 string...");
+            // string compressedBase64 = FileToBase64String(TempFileName);
+            //
+            // Console.WriteLine("Saving...");
+            // WriteBase64String(compressedBase64);
+            // Console.WriteLine("Done.");
         }
 
         /// <summary>
         /// Сохранить base64 строку (преобразованный оригинальный файл) в файл
         /// </summary>
-        /// <param name="source">Куда сохранить base64 строку</param>
         /// <param name="base64String">Преобразованный в base64 оригинальный файл</param>
-        private void WriteBase64String(string base64String)
+        private void SaveToClipboard(string base64String)
         {
-            if (Source == Source.Buffer)
-                Clipboard.SetText(base64String, TextDataFormat.Text);
-            else
-                File.WriteAllText(Base64FilePath, base64String);
-        }
-
-        private string ConvertToBase64String(byte[] bytes)
-        {
-            byte[] Packet = new byte[4096];
-            string b64str = "";
-            using (FileStream fs = new FileStream(file, FileMode.Open))
-            {
-                int i = Packet.Length;
-                while (i == Packet.Length)
-                {
-                    i = fs.Read(Packet, 0, Packet.Length);
-                    b64str = Convert.ToBase64String(Packet, 0, i);
-                }
-            }
+            Clipboard.SetText(base64String, TextDataFormat.Text);
         }
     }
 
@@ -146,61 +91,34 @@ namespace FileEncoder
     /// </summary>
     public class DecodeCommand : ConvertCommand
     {
-        public DecodeCommand(Source source, string filePath) : base(source, filePath) { }
+        public DecodeCommand(string filePath) : base(filePath) { }
 
         /// <inheritdoc/>
         public override void Execute()
         {
-            if (Source == Source.File)
-            {
-                BinaryFilePath = GetBinaryFilePath();
-                Base64FilePath = FilePath;
-                
-                if (!File.Exists(Base64FilePath))
-                {
-                    throw new Exception($"File {Base64FilePath} not found");
-                }
-            }
-            else
-            {
-                BinaryFilePath = FilePath;
-            }
-
-            Console.WriteLine("Reading base64 string of compressed file...");
-            string compressedBase64String = ReadBase64String();
-            Console.WriteLine("Converting to bytes...");
-            byte[] compressedFile = Convert.FromBase64String(compressedBase64String);
-            Console.WriteLine("Decompressing to base64 string...");
-            string base64String = Compressor.Unzip(compressedFile);
-            Console.WriteLine("Converting to bytes...");
-            byte[] file = Convert.FromBase64String(base64String);
-            
-            Console.WriteLine("Saving file...");
-            File.WriteAllBytes(BinaryFilePath, file);
-            Console.WriteLine("Done.");
-        }
-
-        /// <summary>
-        /// Получить путь, куда сохранить оригинальный файл после преобразования
-        /// </summary>
-        /// <param name="filePath">Путь до файла с base64 строкой</param>
-        /// <returns>Путь до будущего ориггинального файла</returns>
-        private string GetBinaryFilePath()
-        {
-            string fileName = Path.GetFileNameWithoutExtension(BinaryFilePath).Replace("_", ".");
-            return Path.Combine(new[] {Path.GetPathRoot(fileName), fileName});
+            // Console.WriteLine("Reading base64 string of compressed file...");
+            // string compressedBase64String = ReadBase64StringFromClipboard();
+            // Console.WriteLine("Converting to bytes...");
+            // byte[] compressedFile = Convert.FromBase64String(compressedBase64String);
+            // Console.WriteLine("Decompressing to base64 string...");
+            // Compressor.Unzip(compressedFile);
+            // Console.WriteLine("Reading uncompressed file base64 string...");
+            // string base64String = FileToBase64String("");
+            // Console.WriteLine("Converting to bytes...");
+            // byte[] file = Convert.FromBase64String(base64String);
+            //
+            // Console.WriteLine("Saving file...");
+            // File.WriteAllBytes(FilePath, file);
+            // Console.WriteLine("Done.");
         }
 
         /// <summary>
         /// Считать base64 строку
         /// </summary>
-        /// <param name="source">Откуда считать base64 строку</param>
         /// <returns>base64 строку - закодированный файл</returns>
-        private string ReadBase64String()
+        private string ReadBase64StringFromClipboard()
         {
-            return Source == Source.Buffer 
-                ? Clipboard.GetText(TextDataFormat.Text) 
-                : File.ReadAllText(Base64FilePath);
+            return Clipboard.GetText(TextDataFormat.Text);
         }
     }
 
@@ -221,9 +139,7 @@ namespace FileEncoder
                 return new EmptyCommand();
             }
 
-            Source source = Source.Buffer;
             string fileName;
-
             if (args.Length == 1)
             {
                 switch (args[0])
@@ -237,7 +153,7 @@ namespace FileEncoder
                     default:
                         if (File.Exists(args[0]))
                         {
-                            return new EncodeCommand(Source.Buffer, args[0]);
+                            return new EncodeCommand(args[0]);
                         }
 
                         return new EmptyCommand();
@@ -246,7 +162,6 @@ namespace FileEncoder
 
             try
             {
-                source = GetSource(args);
                 fileName = GetFileName(args);
             }
             catch (NoFilePathException)
@@ -264,39 +179,15 @@ namespace FileEncoder
 
             if (args.Contains("encode"))
             {
-                return new EncodeCommand(source, fileName);
+                return new EncodeCommand(fileName);
             }
 
             if (args.Contains("decode"))
             {
-                return new DecodeCommand(source, fileName);
+                return new DecodeCommand(fileName);
             }
 
-            return new EncodeCommand(source, fileName);
-        }
-
-        /// <summary>
-        /// Получить источник работы
-        /// </summary>
-        /// <param name="args">Параметры команды</param>
-        /// <returns>Источник, откуда команда должна считывать данные</returns>
-        private static Source GetSource(string[] args)
-        {
-            if (args.Contains("-s") || args.Contains("--source"))
-            {
-                int index = Array.IndexOf(args, "-s");
-                if (index == -1) index = Array.IndexOf(args, "--source");
-
-                string str = args[index + 1];
-                if (str != "buffer" && str != "file")
-                {
-                    throw new UnknownSourceException();
-                }
-
-                return str == "buffer" ? Source.Buffer : Source.File;
-            }
-
-            return Source.Buffer;
+            return new EncodeCommand(fileName);
         }
 
         /// <summary>
@@ -324,7 +215,7 @@ namespace FileEncoder
         static void Main(string[] args)
         {
             args = new[] {"datagrip-2021.1.1.exe"};
-            
+
             var command = CommandFactory.GetCommand(args);
             Console.WriteLine($"Created {command.GetType().Name}.");
             command.Execute();
@@ -353,11 +244,10 @@ namespace FileEncoder
         {
             Console.WriteLine("\t-h|--help\t\tHelp information");
             Console.WriteLine("\t-v|--version -v\t\tProgram version");
-            Console.WriteLine("\t-s|--source -s\t\tsource of input: buffer or file");
             Console.WriteLine("\t-f|--file -f\t\tOutput file name");
             Console.WriteLine("\nExamples:");
-            Console.WriteLine("\tFileEncoder -s file -c encode -f filename.ext");
-            Console.WriteLine("\tFileEncoder --source buffer --command decode -file filename_ext.txt");
+            Console.WriteLine("\tFileEncoder file -c encode -f filename.ext");
+            Console.WriteLine("\tFileEncoder --command decode -file filename_ext.txt");
         }
     }
 
@@ -394,54 +284,98 @@ namespace FileEncoder
             Console.WriteLine(message);
         }
     }
-    
+
     public static class Compressor
     {
         private const string TempFileName = "temp.bin";
-        
+
         // Сохраняем поток в файл, чтобы избежать OutOfMemoryException при выполнении mso.ToArray()
-        
-        public static byte[] Zip(string str) {
-            var bytes = Encoding.UTF8.GetBytes(str);
+
+        public static void Zip(string base64File, string compressedFileName)
+        {
+            var base64String = File.ReadAllText(base64File);
+            var bytes = Encoding.UTF8.GetBytes(base64String);
 
             using (var msi = new MemoryStream(bytes))
-            using (var mso = new FileStream(TempFileName, FileMode.Create)) {
-                using (var gs = new GZipStream(mso, CompressionMode.Compress)) {
+            using (var mso = new FileStream(compressedFileName, FileMode.Create))
+            {
+                using (var gs = new GZipStream(mso, CompressionMode.Compress))
+                {
                     CopyTo(msi, gs);
                 }
 
                 mso.Close();
             }
-
-            byte[] result = File.ReadAllBytes(TempFileName);
-            File.Delete(TempFileName);
-            return result;
         }
 
-        public static string Unzip(byte[] bytes) {
+        public static void Unzip(byte[] bytes)
+        {
             using (var msi = new MemoryStream(bytes))
-            using (var mso = new FileStream(TempFileName, FileMode.Create)) {
-                using (var gs = new GZipStream(msi, CompressionMode.Decompress)) {
+            using (var mso = new FileStream(TempFileName, FileMode.Create))
+            {
+                using (var gs = new GZipStream(msi, CompressionMode.Decompress))
+                {
                     CopyTo(gs, mso);
                 }
 
                 mso.Close();
             }
-            
-            byte[] result = File.ReadAllBytes(TempFileName);
-            File.Delete(TempFileName);
-            
-            return Encoding.UTF8.GetString(result);
         }
-        
-        private static void CopyTo(Stream src, Stream dest) {
+
+        private static void CopyTo(Stream src, Stream dest)
+        {
             byte[] bytes = new byte[4096];
 
             int cnt;
 
-            while ((cnt = src.Read(bytes, 0, bytes.Length)) != 0) {
+            while ((cnt = src.Read(bytes, 0, bytes.Length)) != 0)
+            {
                 dest.Write(bytes, 0, cnt);
             }
+        }
+    }
+    
+    public static class Base64Converter
+    {
+        public static void FileToBase64String(string filePath, string outputFilePath)
+        {
+            using (FileStream fs = File.Open(outputFilePath, FileMode.Create))
+            using (var cs = new CryptoStream(fs, new ToBase64Transform(), CryptoStreamMode.Write))
+            using (var fi = File.Open(filePath, FileMode.Open))
+            {
+                fi.CopyTo(cs);
+            }
+        }
+
+        public static void Base64ToFile(string filePath)
+        {
+            string outputFilePath = filePath + ".base64";
+            
+            using (FileStream f64 = File.Open(outputFilePath, FileMode.Open))
+            using (var cs = new CryptoStream(f64, new FromBase64Transform(), CryptoStreamMode.Read))
+            using (var fo = File.Open(filePath + ".orig", FileMode.Create))
+            {
+                cs.CopyTo(fo);
+            }
+        }
+        
+        public static string FileToBase64String222(string fileName)
+        {
+            byte[] packet = new byte[10 * 1024 * 1024];
+            StringBuilder b64sb = new StringBuilder();
+            int j = 0;
+            using (FileStream fs = new FileStream(fileName, FileMode.Open))
+            {
+                int i = packet.Length;
+                while (i == packet.Length)
+                {
+                    Console.Write($"\rReading {j++} of {fs.Length / packet.Length}");
+                    i = fs.Read(packet, 0, packet.Length);
+                    b64sb.Append(Convert.ToBase64String(packet, 0, i));
+                }
+            }
+
+            return b64sb.ToString();
         }
     }
 
